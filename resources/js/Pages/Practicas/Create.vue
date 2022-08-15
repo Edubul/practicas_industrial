b7e2d97af5994dab0523ae4083b92ab3c21c9481
Eduardo Bueno <edubul23@gmail.com>
2022-08-15 14:47:16 -0500

    Revert "Update Create.vue"
    This reverts commit 263a89d19c52123507680d5fc6da3deaae4e289f.

--- resources/js/Pages/Practicas/Create.vue
+++ resources/js/Pages/Practicas/Create.vue
@@ -240,10 +240,7 @@ function changeIntegrador() {
                                                     <span
                                                         class="px-4 py-2 rounded-full text-gray-500 bg-gray-200 font-semibold text-sm flex align-center w-max cursor-pointer active:bg-gray-300 transition duration-300 ease"
                                                     >
-                                                        {{
-                                                            profesor.last_name
-                                                        }}
-                                                        {{ profesor.name }}
+                                                        {{ profesor }}
 
                                                         <button
                                                             @click="
